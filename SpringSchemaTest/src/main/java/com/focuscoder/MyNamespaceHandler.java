bc59367efdf1ce1f19470297bf2afa85e3a47cc3
Wang Zhihao <wangzhi0571@msn.com>
2016-05-29 23:51:43 +0800

    Spring Customized Schema Example

---
+++ SpringSchemaTest/src/main/java/com/focuscoder/MyNamespaceHandler.java
@@ -0,0 +1,9 @@
+package com.focuscoder;
+
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+public class MyNamespaceHandler extends NamespaceHandlerSupport {
+	public void init() {
+		registerBeanDefinitionParser("people", new PeopleBeanDefinitionParser());
+	}
+}
---
+++ SpringSchemaTest/src/test/java/com/focuscoder/SpringSchemaTest/CustomizedSpringSchemeTest.java
@@ -0,0 +1,28 @@
+package com.focuscoder.SpringSchemaTest;
+
+import static org.junit.Assert.*;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.focuscoder.People;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:applicationContext.xml")
+public class CustomizedSpringSchemeTest {
+
+	@Test
+	public void test() 
+	{  
+		assertEquals("cutesource", p.getId());
+		assertEquals("Wang Zhihao", p.getName());
+		assertEquals(27, p.getAge());
+	}
+	
+	@Resource(name = "cutesource")
+	private People p;
+}
